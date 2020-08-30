8898bd7ad90935b459458773d5542eec574f78ba
John Alexander <jalexander.qed@gmail.com>
2020-08-30 08:12:26 +0000

    Add an advanced ping page with a graph of percentile ping times.

--- src/main/webapp/WEB-INF/jsp/ping_advanced.jsp
+++ src/main/webapp/WEB-INF/jsp/ping_advanced.jsp
@@ -4,11 +4,25 @@
 <head>
   <jsp:include page="/WEB-INF/jsp/lib/head.jsp"/>
   <link rel="stylesheet" href="/css/ping_advanced.css">
+
+<!-- Load c3.css -->
+<link rel="stylesheet" href="/css/c3/c3.css">
+
+<!-- Load d3.js and c3.js -->
+<script src="/js/d3/d3.min.js" charset="utf-8"></script>
+<script src="/js/c3/c3.min.js"></script>
 </head>
 
 <body>
 
 <jsp:include page="/WEB-INF/jsp/lib/navbar.jsp"/>
+
+<div id="ping-chart"></div>
+<br>
+<br>
+<div class="d-flex justify-content-center">
+    <h3 id="ping-rate"></h3>
+</div>
 
 <jsp:include page="/WEB-INF/jsp/lib/foot.jsp"/>
 <script src="js/ping_advanced.js"></script>
---
+++ src/main/webapp/js/ping_advanced.js
@@ -0,0 +1,131 @@
+let kBucketPeriod = 6000;
+let kPingPeriod = 50;
+let kMaxBuckets = 40;
+
+let chart = c3.generate({
+    bindto: "#ping-chart",
+    data: {
+        x: "periodEnd",
+        xFormat: "%Y-%m-%dT%H:%M:%S.%LZ",
+        rows: [
+            ["periodEnd", "50%", "95%", "99%", "max"]
+        ]
+    },
+    transition: {
+        duration: 0,
+    },
+    axis: {
+        x: {
+            type: 'timeseries',
+            tick: {
+                format: '%H:%M:%S'
+            },
+            label: "Time at End of Window"
+        },
+        y: {
+            label: "Ping Time (ms)",
+        }
+    },
+    grid: {
+        y: {
+            show: true
+        }
+    },
+});
+
+function DataBucket(startDate) {
+    this.startDate = startDate;
+    this.pings = [];
+    this.sorted = false;
+}
+
+DataBucket.prototype.totalRequests = function() {
+    return this.pings.length;
+}
+
+DataBucket.prototype.push = function(ping) {
+    this.sorted = false;
+    this.pings.push(ping);
+}
+
+// Returns a data row consisting of the bucket's start date and
+// it's 50th, 95th, 99th, and 100th percentile ping times. Returns
+// null if there is no data for this time period.
+DataBucket.prototype.getRowData = function() {
+    if(this.pings.length == 0) {
+        return null;
+    }
+    if(!this.sorted) {
+        this.pings.sort(function(a, b) { return a - b; });
+    }
+    this.sorted = true;
+    return [
+        new Date(this.startDate.getTime() + kBucketPeriod).toISOString(),
+        this.pings[Math.floor(0.5 * this.pings.length)],
+        this.pings[Math.floor(0.95 * this.pings.length)],
+        this.pings[Math.floor(0.99 * this.pings.length)],
+        this.pings[Math.floor(this.pings.length - 1)],
+    ];
+}
+
+// Array of Bucket() objects.
+let buckets = [];
+
+function updatePingRate() {
+    let qps = Math.floor(1000 / kPingPeriod);
+    if(buckets.length > 5) {
+        qps = buckets[buckets.length - 3].totalRequests() / (kBucketPeriod / 1000);
+    }
+    document.getElementById("ping-rate").innerHTML =
+        "Testing at " + qps.toFixed(1) + " QPS with a " +
+        (kBucketPeriod / 1000) + " second window";
+}
+
+function updateChart() {
+    while(buckets.length > kMaxBuckets) {
+        buckets.shift();
+    }
+    let rows = [["periodEnd", "50%", "95%", "99%", "max"]];
+    buckets.forEach(function(bucket) {
+        let row = bucket.getRowData();
+        if(row !== null) {
+            rows.push(row);
+        }
+    });
+    chart.load({
+        rows: rows
+    });
+}
+
+// Adds a new element to `buckets` aligned to duration
+// kBucketPeriod. `now` must be of type `Date`.
+function addNewBucket(now) {
+    let alignedMillis = now.getTime() - (now.getTime() % kBucketPeriod);
+    buckets.push(new DataBucket(new Date(alignedMillis)));
+    setTimeout(updateChart, 1000);
+}
+
+function recordError(error) {
+    console.log(error);
+}
+
+function performPing() {
+    let now = new Date();
+    if (buckets.length == 0 ||
+        buckets[buckets.length - 1].startDate.getTime() + kBucketPeriod < now.getTime()) {
+        addNewBucket(now);
+    }
+    let currentBucket = buckets[buckets.length - 1];
+    let start_time = performance.now();
+    let ping_time = 0.0;
+    fetch("/api/ping")
+        .then(response => {
+            ping_time = performance.now() - start_time;
+            return response.json();
+        })
+        .then(data => currentBucket.push(ping_time))
+        .catch(error => recordError(error));
+}
+
+let intervalID = setInterval(performPing, kPingPeriod);
+let pingRateIntervalID = setInterval(updatePingRate, kPingPeriod);
