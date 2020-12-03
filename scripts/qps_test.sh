4781c156ecf63c964848f15feadbf6ce8d7f9f2c
John Alexander <jalexander.qed@gmail.com>
2020-12-03 08:29:36 +0000

    Add comment and minor improvement to QPS script.

--- scripts/qps_test.sh
+++ scripts/qps_test.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
+
+# Hits a URL (first argument) serially for 10 seconds,
+# then prints QPS. Not very precise and usually requires
+# checking that "curl -s $1 > /dev/null" works as expected
+# first (i.e. is not a redirect).
 end=$((SECONDS+10))
+
+# One request to warm it up.
+curl -s $1 > /dev/null
 
 let count=0;
 while [ $SECONDS -lt $end ]; do
